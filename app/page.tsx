082646f8637a163cd1ad66bf6a736dcbb5fc87a0
Misha Wagner <mishajw@gmail.com>
2024-03-29 17:01:31 +0000

    Clean up.

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import ModelPlot from './modelPlot';
 import { useState } from 'react';
-import { Model, ModelFields } from '@/lib/model';
+import { ModelFields } from '@/lib/model';
 import CustomModelEditor from './customModelEditor';
 import { MODELS } from '@/lib/dataset';
 import { PARAMETERS } from '@/lib/parameters';
@@ -16,14 +16,28 @@ export default function PlotView() {
     numParams: { value: PARAMETERS['numParams']!.default, source: 'custom' },
   });
   return (
-    <div className='flex flex-row flex-wrap justify-center items-start'>
-      <div className='m-2 p-2 border-2'>
-        <CustomModelEditor fields={customFields} setFields={setCustomFields} />
-        <MethodDescriptions />
-        <ScaleExplanation />
+    <div className='flex flex-col items-center'>
+      <div className='m-2 p-2 border-2 max-w-screen-md'>
+        A tool for exploring the relationships between large language model
+        attributes. This should help you get a feel for them, for example how
+        dataset size and FLOPs are related.
       </div>
-      <div className='m-2 p-2 border-2'>
-        <ModelPlot customFields={customFields} models={MODELS} />
+      <div className='flex flex-row flex-wrap justify-center items-start'>
+        <div>
+          <div className='m-2 p-2 border-2'>
+            <CustomModelEditor
+              fields={customFields}
+              setFields={setCustomFields}
+            />
+          </div>
+          <div className='m-2 p-2 border-2'>
+            <MethodDescriptions />
+            <ScaleExplanation />
+          </div>
+        </div>
+        <div className='m-2 p-2 border-2'>
+          <ModelPlot customFields={customFields} models={MODELS} />
+        </div>
       </div>
     </div>
   );
--- app/scaleExplanation.tsx
+++ app/scaleExplanation.tsx
@@ -6,7 +6,7 @@ export default function ScaleExplanation() {
     power: i * 3,
   })).filter(({ symbol }) => symbol !== '');
   return (
-    <div className='flex justify-center text-sm text-gray-600 italic mt-2'>
+    <div className='flex justify-center text-sm text-gray-600 italic mt-10'>
       {powers.map(({ symbol, power }, i) => (
         <span key={symbol}>
           {symbol}=10<sup>{power}</sup>
