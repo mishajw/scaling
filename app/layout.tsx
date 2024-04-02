c3812c538a238fc1e116211658496bea21438fcf
Misha Wagner <mishajw@gmail.com>
2024-04-01 20:08:06 -0700

    Add suspense.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
 import Header from './header';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.className}>
-        <Header>{children}</Header>
+        <Header>
+          <Suspense>{children}</Suspense>
+        </Header>
       </body>
     </html>
   );
