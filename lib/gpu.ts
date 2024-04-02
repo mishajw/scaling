7aa6573811205cd9a178d6ee5facbab7b17d5457
Misha Wagner <mishajw@gmail.com>
2024-04-01 19:37:29 -0700

    Add GPU values.

--- lib/gpu.ts
+++ lib/gpu.ts
@@ -5,7 +5,6 @@ export default interface Gpu {
   type: GpuType;
   flopsPerSecond: number;
   costDollarsPerHour: number;
-  memoryGb: number;
 }
 
 export const GpuTypeSchema = z.union([
@@ -14,11 +13,13 @@ export const GpuTypeSchema = z.union([
   z.literal('Google TPU v3'),
   z.literal('Google TPU v4'),
   z.literal('Google TPU v4,Google TPU v3'),
+  z.literal('NVIDIA A40'),
   z.literal('NVIDIA A100'),
   z.literal('NVIDIA V100'),
   z.literal('NVIDIA A100 SXM4 40 GB'),
   z.literal('NVIDIA A100 SXM4 80 GB'),
   z.literal('NVIDIA A800'),
+  z.literal('NVIDIA H100'),
   z.literal('NVIDIA H100 SXM5'),
   z.literal('NVIDIA Tesla V100 DGXS 32 GB'),
   z.literal('NVIDIA Tesla V100 SXM2'),
@@ -29,26 +30,27 @@ export type GpuType = z.infer<typeof GpuTypeSchema>;
 export const GPU_TYPES: Partial<Record<GpuType, Gpu>> = {
   'Google TPU v4': {
     type: 'Google TPU v4',
-    flopsPerSecond: siParse('1T')!,
-    costDollarsPerHour: 2,
-    memoryGb: 40,
-  },
-  'NVIDIA A100': {
-    type: 'NVIDIA A100',
-    flopsPerSecond: siParse('1T')!,
-    costDollarsPerHour: 2,
-    memoryGb: 40,
+    flopsPerSecond: siParse('275T')!,
+    costDollarsPerHour: 2.0286,
   },
   'NVIDIA V100': {
     type: 'NVIDIA V100',
-    flopsPerSecond: siParse('1T')!,
-    costDollarsPerHour: 2,
-    memoryGb: 40,
+    flopsPerSecond: siParse('119T')!,
+    costDollarsPerHour: 0.8,
   },
-  'NVIDIA H100 SXM5': {
-    type: 'NVIDIA H100 SXM5',
-    flopsPerSecond: siParse('1T')!,
-    costDollarsPerHour: 2,
-    memoryGb: 40,
+  'NVIDIA A40': {
+    type: 'NVIDIA A40',
+    flopsPerSecond: siParse('149T')!,
+    costDollarsPerHour: 1.28,
+  },
+  'NVIDIA A100': {
+    type: 'NVIDIA A100',
+    flopsPerSecond: siParse('312T')!,
+    costDollarsPerHour: 2.21,
+  },
+  'NVIDIA H100': {
+    type: 'NVIDIA H100',
+    flopsPerSecond: siParse('989T')!,
+    costDollarsPerHour: 4.76,
   },
 };
